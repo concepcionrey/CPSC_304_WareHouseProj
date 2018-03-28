d91c76a6065ffd1a72a08f674677f07fa904ed1e
beharh <behar.hyseni@alumni.ubc.ca>
2018-03-28 13:48:38 -0700

    Added the textbox input restrictions

--- ClientView.php
+++ ClientView.php
@@ -25,9 +25,9 @@
 
 <div id = "queriesClient">
     <form action="insert_order_client.php", method="get">
-        Item IID:<input type="text" name="IIDOrder" id="IIDOrder">
-        Client CLID:<input type="text" name="CLIDOrder" id="CLIDOrder">
-        Order OD:<input type="text" name="ODOrder" id="ODOrder">
+        Item IID:<input type="text" name="IIDOrder"  maxlength="15" onkeypress='return event.charCode >=48 && event.charCode<=57'   title="Please enter numbers only."  placeholder="Enter numbers only." id="IIDOrder">
+        Client CLID:<input type="text" name="CLIDOrder"  maxlength="15" onkeypress='return event.charCode >=48 && event.charCode<=57'   title="Please enter numbers only."  placeholder="Enter numbers only." id="CLIDOrder">
+        Order OD:<input type="text" name="ODOrder"  maxlength="15" onkeypress='return event.charCode >=48 && event.charCode<=57'   title="Please enter numbers only."  placeholder="Enter numbers only." id="ODOrder">
         <button type="add item" id="addItemButton">Order Item</button> <br>
         <script>
             document.getElementById("CLIDOrder").value = document.cookie.split(";")[0];
@@ -35,19 +35,19 @@
     </form>
     
     <form action="cancel_order_client.php", method="get">
-        Order ID: <input type="text" name="cancelOrderID", id="cancelOrderID"> 
+        Order OD: <input type="text" name="cancelOrderID",  maxlength="15" onkeypress='return event.charCode >=48 && event.charCode<=57'   title="Please enter numbers only."  placeholder="Enter numbers only." id="cancelOrderID">
         <button type="add item" id="updateStockButton">Cancel Order</button> <br>
     </form>
     
     <form action="update_creditCardNumber_client.php", method="get">
-        New Credit Card#: <input type="text" name="creditCardNumberUpdate" id="creditCardNumberUpdate"> 
-        Client CLID:<input type="text" name="CLIDupdate" id="CLIDupdate">
+        New Credit Card#: <input type="text" name="creditCardNumberUpdate"  maxlength="16" onkeypress='return event.charCode >=48 && event.charCode<=57'   title="Please enter numbers only."  placeholder="Enter numbers only." id="creditCardNumberUpdate">
+        Client CLID:<input type="text" name="CLIDupdate"   maxlength="15" onkeypress='return event.charCode >=48 && event.charCode<=57'   title="Please enter numbers only."  placeholder="Enter numbers only." id="CLIDupdate">
         <button type="add item" id="updateCreditCard">Update credit card info</button> <br>
     </form>
     
     <form action="update_email_client.php", method="get">
-        New email: <input type="text" name="emailUpdate" id="emailUpdate"> 
-        Client CLID:<input type="text" name="CLIDupdate" id="CLIDupdate">
+        New email: <input type="text" name="emailUpdate" maxLength="40" title="Please enter your email address."  placeholder="Enter your email address." id="emailUpdate">
+        Client CLID:<input type="text" name="CLIDupdate"  maxlength="15" onkeypress='return event.charCode >=48 && event.charCode<=57'   title="Please enter numbers only."  placeholder="Enter numbers only." id="CLIDupdate">
         <button type="add item" id="updateEmail">Update Email</button> <br>
     </form>
     
@@ -61,7 +61,7 @@
             <option value=">=">&ge;</option>
             <option value="<=">&le;</option>
         </select> 
-        <input type="text" name="retrievePrice">
+        <input type="number" step="0.01" onkeypress="if(this.value.length==15) return false;" min="0.01" max="999999999999999"  title="Please enter numbers only." placeholder="Enter numbers only."  name="retrievePrice">
         <button type="add item" id="selectItemsPriceButton">Select Items</button> <br>
     </form>
 </div>
